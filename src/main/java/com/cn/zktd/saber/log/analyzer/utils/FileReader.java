3d91551b3bacbdd3ef0f8ab053ce7c87c2082cb0
home45006 <home45006@126.com>
2018-08-17 12:30:22 +0800

    开发

--- src/main/java/com/cn/zktd/saber/log/analyzer/utils/FileReader.java
+++ src/main/java/com/cn/zktd/saber/log/analyzer/utils/FileReader.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -22,21 +23,37 @@ public class FileReader {
 //            String fileName = "F:\\tmp\\log.txt";
             String fileName = "F:\\tmp\\test.log";
 
-            Pattern p= Pattern.compile("([a-z]+)(\\d+)");
+            Pattern p= Pattern.compile(".*B2\\|\\|<<<<<-------.*");
 
             List<String> lineLists = Files
                     .lines(Paths.get(fileName), Charset.defaultCharset())
                     .flatMap(line -> Arrays.stream(line.split("\n")))
-                    .anyMatch(line -> p.matcher(line).find())
                     .collect(Collectors.toList());
 
             //输出文件函数
             System.out.println("lineLists====" + lineLists.size());
 
             //输出每一行文件内容
-            lineLists.stream().forEach(System.out::println);
+            lineLists.stream().forEach(line -> {
+                if(!line.contains("||PingRequest") && !line.contains("||GetStatusRequest") && !line.contains("||ConnectRequest")) {
+                    if(line.contains("B2||<<<<<-------")) {
+                        System.out.println(line);
+                    }
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Pattern p= Pattern.compile(".*B2\\|\\|<<<<<-------.*");
+        String line = "2018-07-30 11:22:36.741 [B2-work-59] INFO  c.c.z.saber.server.local.B2.handler.B2BaseHandler - B2||<<<<<-------6689ac0d7ceb4f6aa87add08a2fcedb8||GetStatusRequest ROM1708310008959-53670-GREEN_CIRCLE";
+        Matcher m = p.matcher(line);
+        if(m.find()) {
+            System.out.println(m.group(0));
+        } else {
+            System.out.println("No Match");
         }
     }
 }
